32245789e98c56bbf2c9032221cc6808a4f72ddc
david <david@fries.net>
1999-07-06 01:04:46 +0000

    display the hex data of the file inthe correct order.

---
+++ misc/hex.cc
@@ -0,0 +1,31 @@
+#include <fstream.h>
+#include <iomanip.h>
+#include <stdlib.h>
+
+int main( int argc, char **argv)
+{
+	ifstream input( *(argv+1) );
+	int byte;
+	int linefeed = 0;
+	if( !input )
+	{
+		cerr << "Error\n";
+		exit(1);
+	}
+	while( !input.eof() )
+	{
+		byte = input.get();
+		cout << hex << setfill('0') << setw(2) << byte;
+		if( linefeed )
+		{
+			cout << '\n';
+			linefeed = 0;
+		}
+		else
+		{
+			linefeed = 1;
+		}
+	}
+	cout << endl;
+	return 0;
+}
